1606ef15702ac5c9a5d0316d7b4a206caa05779a
Marco <marco.fura@unmsm.edu.pe>
2022-04-24 15:38:11 -0500

    other issue solved

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,4 +1,10 @@
+import Head from "next/head";
 import "../styles/globals.css";
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return<>
+    <Head>
+        <title>Carnet - UPC</title>
+    </Head>
+  <Component {...pageProps} />
+  </>
 }
--- pages/_document.jsx
+++ pages/_document.jsx
@@ -5,8 +5,39 @@ export default function MyDocument(){
             <Head>
                 <link rel="manifest" href="/manifest.json" />
                 <link rel="apple-touch-icon" href="/icon.png"></link>
-                <meta name="theme-color" content="#e8432d" />
-                <title>Carnet - UPC</title>
+               
+               <meta name='application-name' content='PWA UPC' />
+<meta name='apple-mobile-web-app-capable' content='yes' />
+<meta name='apple-mobile-web-app-status-bar-style' content='default' />
+<meta name='apple-mobile-web-app-title' content='PWA UPC' />
+<meta name='description' content='PWA para carnets electronicos' />
+<meta name='format-detection' content='telephone=no' />
+<meta name='mobile-web-app-capable' content='yes' />
+<meta name='msapplication-config' content='/icons/browserconfig.xml' />
+<meta name='msapplication-TileColor' content='#2B5797' />
+<meta name='msapplication-tap-highlight' content='no' />
+<meta name='theme-color' content='#e8432d' />
+
+<link rel='apple-touch-icon' href='/icons/touch-icon-iphone.png' />
+<link rel='apple-touch-icon' sizes='192x192' href='/icon-192x192.png' />
+<link rel='apple-touch-icon' sizes='256x256' href='/icon-256x256.png' />
+<link rel='apple-touch-icon' sizes='384x384' href='/icon-384x384.png' />
+
+<link rel='icon' type='image/png' sizes='512x512' href='/icon-512x512.png' />
+<link rel='manifest' href='/manifest.json' />
+     
+<meta name='twitter:card' content='summary' />
+<meta name='twitter:url' content='https://vasacaer.com' />
+<meta name='twitter:title' content='PWA App' />
+<meta name='twitter:description' content='Aplicacion recreativa' />
+<meta name='twitter:image' content='https://vasacaer.com/icons/android-chrome-192x192.png' />
+<meta name='twitter:creator' content='@Sideral' />
+<meta property='og:type' content='website' />
+<meta property='og:title' content='PWA Carnet - UPC' />
+<meta property='og:description' content='Best PWA App in the world' />
+<meta property='og:site_name' content='PWA Carnet UPC' />
+<meta property='og:url' content='https://vasacaer.com' />
+<meta property='og:image' content='https://vasacaer.com/icons/apple-touch-icon.png' />
             </Head>
             <body>
                 <Main/>
