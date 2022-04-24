8a3cb45c479327ba871f9e46ba96b2a4dfaf462c
Marco <marco.fura@unmsm.edu.pe>
2022-04-24 14:41:58 -0500

    second release

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 const withPWA=require("next-pwa");
 module.exports=withPWA({
     pwa:{
-        dest:"public"
+        dest:"public",
+        register: true,
+        skipWaiting: true,
     }
 })
