28c68afe90e7f520a5cd45fd284fef3cf1f228a5
Marco <marco.fura@unmsm.edu.pe>
2022-04-24 14:31:36 -0500

    manifest added

---
+++ pages/_document.jsx
@@ -0,0 +1,16 @@
+import {Head, Html, Main, NextScript} from "next/document";
+export default function MyDocument(){
+    return(
+        <Html>
+            <Head>
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png"></link>
+                <meta name="theme-color" content="#e8432d" />
+            </Head>
+            <body>
+                <Main/>
+                <NextScript/>
+            </body>
+        </Html>
+    )
+}
